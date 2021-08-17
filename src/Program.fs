a5c2c072619f77ac27206526d5f17884df8ba652
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2021-08-16 23:47:41 -0300

    feat: setup fslex and fsyacc

---
+++ src/Program.fs
@@ -0,0 +1,18 @@
+module Program
+
+open FSharp.Text.Lexing
+open Lexer
+
+let parse input =
+  let ast =
+    LexBuffer<char>.FromString input
+    |> Lexer.tokenstream
+
+  ast
+
+
+[<EntryPoint>]
+let main _ =
+  printfn "%A" (parse "{")
+
+  0
