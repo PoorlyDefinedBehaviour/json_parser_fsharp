61aeb034c471f31c0ff35c64237627fc5a1e2020
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2021-08-17 21:10:18 -0300

    fix(Parser): parse object with multiple properties

--- src/Parser.fs
+++ src/Parser.fs
@@ -127,16 +127,16 @@ let _fsyacc_dataOfToken (t:token) =
   | INT _fsyacc_x -> Microsoft.FSharp.Core.Operators.box _fsyacc_x 
   | RIGHTBRACE  -> (null : System.Object) 
   | LEFTBRACE  -> (null : System.Object) 
-let _fsyacc_gotos = [| 0us; 65535us; 1us; 65535us; 0us; 1us; 5us; 65535us; 0us; 2us; 8us; 12us; 13us; 14us; 20us; 21us; 25us; 26us; 5us; 65535us; 0us; 6us; 8us; 6us; 13us; 6us; 20us; 6us; 25us; 6us; 1us; 65535us; 8us; 10us; 5us; 65535us; 0us; 7us; 8us; 7us; 13us; 7us; 20us; 7us; 25us; 7us; 2us; 65535us; 15us; 17us; 22us; 23us; 0us; 65535us; |]
-let _fsyacc_sparseGotoTableRowOffsets = [|0us; 1us; 3us; 9us; 15us; 17us; 23us; 26us; |]
-let _fsyacc_stateToProdIdxsTableElements = [| 1us; 0us; 1us; 0us; 1us; 1us; 1us; 2us; 1us; 3us; 1us; 4us; 1us; 5us; 1us; 6us; 2us; 7us; 8us; 1us; 7us; 2us; 8us; 10us; 1us; 8us; 1us; 9us; 1us; 10us; 1us; 10us; 2us; 11us; 12us; 1us; 11us; 1us; 12us; 1us; 12us; 1us; 13us; 1us; 13us; 1us; 13us; 1us; 14us; 1us; 14us; 1us; 14us; 1us; 14us; 1us; 14us; |]
-let _fsyacc_stateToProdIdxsTableRowOffsets = [|0us; 2us; 4us; 6us; 8us; 10us; 12us; 14us; 16us; 19us; 21us; 24us; 26us; 28us; 30us; 32us; 35us; 37us; 39us; 41us; 43us; 45us; 47us; 49us; 51us; 53us; 55us; |]
-let _fsyacc_action_rows = 27
-let _fsyacc_actionTableElements = [|5us; 32768us; 4us; 8us; 5us; 3us; 6us; 5us; 7us; 4us; 9us; 15us; 0us; 49152us; 0us; 16385us; 0us; 16386us; 0us; 16387us; 0us; 16388us; 0us; 16389us; 0us; 16390us; 6us; 32768us; 3us; 9us; 4us; 8us; 5us; 3us; 6us; 5us; 7us; 4us; 9us; 15us; 0us; 16391us; 2us; 32768us; 2us; 13us; 3us; 11us; 0us; 16392us; 0us; 16393us; 5us; 32768us; 4us; 8us; 5us; 3us; 6us; 5us; 7us; 4us; 9us; 15us; 0us; 16394us; 3us; 32768us; 2us; 22us; 5us; 19us; 8us; 16us; 0us; 16395us; 1us; 32768us; 8us; 18us; 0us; 16396us; 1us; 32768us; 1us; 20us; 5us; 32768us; 4us; 8us; 5us; 3us; 6us; 5us; 7us; 4us; 9us; 15us; 0us; 16397us; 2us; 32768us; 2us; 22us; 5us; 19us; 1us; 32768us; 5us; 24us; 1us; 32768us; 1us; 25us; 5us; 32768us; 4us; 8us; 5us; 3us; 6us; 5us; 7us; 4us; 9us; 15us; 0us; 16398us; |]
-let _fsyacc_actionTableRowOffsets = [|0us; 6us; 7us; 8us; 9us; 10us; 11us; 12us; 13us; 20us; 21us; 24us; 25us; 26us; 32us; 33us; 37us; 38us; 40us; 41us; 43us; 49us; 50us; 53us; 55us; 57us; 63us; |]
+let _fsyacc_gotos = [| 0us; 65535us; 1us; 65535us; 0us; 1us; 5us; 65535us; 0us; 2us; 8us; 12us; 13us; 14us; 20us; 21us; 24us; 25us; 5us; 65535us; 0us; 6us; 8us; 6us; 13us; 6us; 20us; 6us; 24us; 6us; 1us; 65535us; 8us; 10us; 5us; 65535us; 0us; 7us; 8us; 7us; 13us; 7us; 20us; 7us; 24us; 7us; 1us; 65535us; 15us; 17us; 0us; 65535us; |]
+let _fsyacc_sparseGotoTableRowOffsets = [|0us; 1us; 3us; 9us; 15us; 17us; 23us; 25us; |]
+let _fsyacc_stateToProdIdxsTableElements = [| 1us; 0us; 1us; 0us; 1us; 1us; 1us; 2us; 1us; 3us; 1us; 4us; 1us; 5us; 1us; 6us; 2us; 7us; 8us; 1us; 7us; 2us; 8us; 10us; 1us; 8us; 1us; 9us; 1us; 10us; 1us; 10us; 2us; 11us; 12us; 1us; 11us; 2us; 12us; 14us; 1us; 12us; 1us; 13us; 1us; 13us; 1us; 13us; 1us; 14us; 1us; 14us; 1us; 14us; 1us; 14us; |]
+let _fsyacc_stateToProdIdxsTableRowOffsets = [|0us; 2us; 4us; 6us; 8us; 10us; 12us; 14us; 16us; 19us; 21us; 24us; 26us; 28us; 30us; 32us; 35us; 37us; 40us; 42us; 44us; 46us; 48us; 50us; 52us; 54us; |]
+let _fsyacc_action_rows = 26
+let _fsyacc_actionTableElements = [|5us; 32768us; 4us; 8us; 5us; 3us; 6us; 5us; 7us; 4us; 9us; 15us; 0us; 49152us; 0us; 16385us; 0us; 16386us; 0us; 16387us; 0us; 16388us; 0us; 16389us; 0us; 16390us; 6us; 32768us; 3us; 9us; 4us; 8us; 5us; 3us; 6us; 5us; 7us; 4us; 9us; 15us; 0us; 16391us; 2us; 32768us; 2us; 13us; 3us; 11us; 0us; 16392us; 0us; 16393us; 5us; 32768us; 4us; 8us; 5us; 3us; 6us; 5us; 7us; 4us; 9us; 15us; 0us; 16394us; 2us; 32768us; 5us; 19us; 8us; 16us; 0us; 16395us; 2us; 32768us; 2us; 22us; 8us; 18us; 0us; 16396us; 1us; 32768us; 1us; 20us; 5us; 32768us; 4us; 8us; 5us; 3us; 6us; 5us; 7us; 4us; 9us; 15us; 0us; 16397us; 1us; 32768us; 5us; 23us; 1us; 32768us; 1us; 24us; 5us; 32768us; 4us; 8us; 5us; 3us; 6us; 5us; 7us; 4us; 9us; 15us; 0us; 16398us; |]
+let _fsyacc_actionTableRowOffsets = [|0us; 6us; 7us; 8us; 9us; 10us; 11us; 12us; 13us; 20us; 21us; 24us; 25us; 26us; 32us; 33us; 36us; 37us; 40us; 41us; 43us; 49us; 50us; 52us; 54us; 60us; |]
 let _fsyacc_reductionSymbolCounts = [|1us; 1us; 1us; 1us; 1us; 1us; 1us; 2us; 3us; 1us; 3us; 2us; 3us; 3us; 5us; 1us; |]
 let _fsyacc_productionToNonTerminalTable = [|0us; 1us; 2us; 2us; 2us; 2us; 2us; 3us; 3us; 4us; 4us; 5us; 5us; 6us; 6us; 7us; |]
-let _fsyacc_immediateActions = [|65535us; 49152us; 16385us; 16386us; 16387us; 16388us; 16389us; 16390us; 65535us; 16391us; 65535us; 16392us; 16393us; 65535us; 16394us; 65535us; 16395us; 65535us; 16396us; 65535us; 65535us; 16397us; 65535us; 65535us; 65535us; 65535us; 16398us; |]
+let _fsyacc_immediateActions = [|65535us; 49152us; 16385us; 16386us; 16387us; 16388us; 16389us; 16390us; 65535us; 16391us; 65535us; 16392us; 16393us; 65535us; 16394us; 65535us; 16395us; 65535us; 16396us; 65535us; 65535us; 16397us; 65535us; 65535us; 65535us; 16398us; |]
 let _fsyacc_reductions ()  =    [| 
 # 141 "src/Parser.fs"
         (fun (parseState : FSharp.Text.Parsing.IParseState) ->
@@ -292,14 +292,14 @@ let _fsyacc_reductions ()  =    [|
                  : 'gentype_object_properties));
 # 293 "src/Parser.fs"
         (fun (parseState : FSharp.Text.Parsing.IParseState) ->
-            let _2 = parseState.GetInput(2) :?> 'gentype_object_properties in
+            let _1 = parseState.GetInput(1) :?> 'gentype_object_properties in
             let _3 = parseState.GetInput(3) :?> string in
             let _5 = parseState.GetInput(5) :?> 'gentype_value in
             Microsoft.FSharp.Core.Operators.box
                 (
                    (
 # 52 "./src/Parser.fsy"
-                                                                      (_3, _5) :: _2 
+                                                                      (_3, _5) :: _1 
                    )
 # 52 "./src/Parser.fsy"
                  : 'gentype_object_properties));
--- src/Program.fs
+++ src/Program.fs
@@ -1,21 +1,76 @@
 module Program
 
 open FSharp.Text.Lexing
+open System
 open Lexer
 open Parser
 
 let parse input =
-  Parser.start Lexer.tokenstream (LexBuffer<char>.FromString input)
+  let lexbuf = LexBuffer<char>.FromString input
+
+  try
+    Parser.start Lexer.tokenstream lexbuf
+  with
+  | e ->
+    let line = lexbuf.EndPos.Line
+    let column = lexbuf.EndPos.Column
+    let lastToken = String lexbuf.Lexeme
+    printf "Parser error at line %d, column %d:\n" line column
+    printf "Last loken: %s\n" lastToken
+    printf "Error: %A\n" e
+    printf "\n"
+    exit 1
 
 [<EntryPoint>]
 let main _ =
   let json =
     """
     {
-      "id": "a04d0943-e0d9-4da1-a0e1-b521851a2ceb"
+      "id": 1,
+      "name": "Leanne Graham",
+      "username": "Bret",
+      "email": "Sincere@april.biz",
+      "address": {
+        "street": "Kulas Light",
+        "suite": "Apt. 556",
+        "city": "Gwenborough",
+        "zipcode": "92998-3874",
+        "geo": {
+          "lat": "-37.3159",
+          "lng": "81.1496"
+        }
+      },
+      "phone": "1-770-736-8031 x56442",
+      "website": "hildegard.org",
+      "company": {
+        "name": "Romaguera-Crona",
+        "catchPhrase": "Multi-layered client-server neural-net",
+        "bs": "harness real-time e-markets"
+      }
     }
     """
 
   printfn "%A" (parse json)
+  // Object
+  // (map
+  //    [("address",
+  //      Object
+  //        (map
+  //           [("city", String "Gwenborough");
+  //            ("geo",
+  //             Object
+  //               (map [("lat", String "-37.3159"); ("lng", String "81.1496")]));
+  //            ("street", String "Kulas Light"); ("suite", String "Apt. 556");
+  //            ("zipcode", String "92998-3874")]));
+  //     ("company",
+  //      Object
+  //        (map
+  //           [("bs", String "harness real-time e-markets");
+  //            ("catchPhrase", String "Multi-layered client-server neural-net");
+  //            ("name", String "Romaguera-Crona")]));
+  //     ("email", String "Sincere@april.biz"); ("id", Int 1);
+  //     ("name", String "Leanne Graham");
+  //     ("phone", String "1-770-736-8031 x56442"); ("username", String "Bret");
+  //     ("website", String "hildegard.org")])
 
   0
